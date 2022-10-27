43818da842c41880ce6dc7f3362bb1b1ee246173
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2022-10-27 18:39:39 +0200

    Update working directory in script

--- R/rss_vws.R
+++ R/rss_vws.R
@@ -1,7 +1,7 @@
 library(tidyRSS)
 
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set to source file location
-setwd("../")
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set to source file location
+#setwd("../")
 
 dat <- tidyfeed("https://feeds.rijksoverheid.nl/ministeries/ministerie-van-volksgezondheid-welzijn-en-sport/kamerstukken.rss", list = T)
 dat <- dat$entries[,1:5]
