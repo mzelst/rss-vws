66930e0ee25a69a26311421bd249fcbc27eff913
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2022-10-27 12:51:53 +0200

    Fix working directory

--- R/rss_vws.R
+++ R/rss_vws.R
@@ -1,5 +1,8 @@
 library(tidyRSS)
 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set to source file location
+setwd("../")
+
 dat <- tidyfeed("https://feeds.rijksoverheid.nl/ministeries/ministerie-van-volksgezondheid-welzijn-en-sport/kamerstukken.rss", list = T)
 dat <- dat$entries[,1:5]
 
