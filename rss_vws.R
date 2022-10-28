94c4013e88d5d305eabed7682f29bf37308599b9
Marino van Zelst <j.m.vanzelst@tilburguniversity.edu>
2022-10-28 09:49:51 +0200

    Include code to aggregate parsed rss feeds

--- rss_vws.R
+++ rss_vws.R
@@ -1,16 +1,18 @@
 library(tidyRSS)
 
-#setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set to source file location
-#setwd("../")
-
 dat <- tidyfeed("https://feeds.rijksoverheid.nl/ministeries/ministerie-van-volksgezondheid-welzijn-en-sport/kamerstukken.rss", list = T)
 dat <- dat$entries[,1:5]
 
 filename.raw.data <- paste0("raw-data/rss-vws_",Sys.Date(),"-",format(Sys.time(), "%H"),"-",format(Sys.time(), "%M"),".csv")
 
-#setwd("../")
+write.csv(dat, file = filename.raw.data, row.names = F)
 
-write.csv(dat, file = filename.raw.data)
+temp = list.files(path = "raw-data/",pattern="*.csv", full.names = T) ## Fetch all day files
+myfiles = lapply(temp, read.csv) ## Load all day files
 
+dat.full <- do.call(rbind,myfiles)
+dat.full <- rbind(dat,dat.full)
 
+dat.full <- unique(dat.full)
 
+write.csv(dat.full, file = "output/kamerstukken_vws.csv")
